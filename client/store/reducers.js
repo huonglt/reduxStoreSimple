1e5de39a1b4a0516944d0aa00c3c93b12dcf978d
huonglt <lthuongvn@yahoo.com>
2017-03-20 22:59:47 +1300

    use switch case to handle action type

--- client/store/reducers.js
+++ client/store/reducers.js
@@ -1,10 +1,9 @@
 import { INCREMENT_ACTION, DECREMENT_ACTION } from './actions.js';
 
 export const reducers = (action, state) => {
-  if(action.type == INCREMENT_ACTION.type) {
-    return {...state, count: state.count + 1};
-  } else if(action.type == DECREMENT_ACTION.type) {
-    return {...state, count: state.count - 1};
+  switch(action.type) {
+    case INCREMENT_ACTION.type: return {...state, count: state.count + 1};
+    case DECREMENT_ACTION.type: return {...state, count: state.count - 1};
+    default: return state;
   }
-  return state;
 }
